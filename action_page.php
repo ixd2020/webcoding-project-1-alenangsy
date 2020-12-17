2d2361bc451f8ee438646e8aa2ae827c209972b2
Glitch (shawnmendes-website) <none>
2020-12-17 17:39:55 +0000

    🍡💈 Updated with Glitch

---
+++ action_page.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    
+Your information have been saved.We will send you the confirmation to your email.
+<?php echo $_POST["fname"]; ?><br>
+<?php echo $_POST["lname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+Country you are from:  <?php echo $_POST["country"]; ?>
+    
+  </body>
+</html>
+    
